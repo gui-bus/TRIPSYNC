c98349faf85e33b700616a5e268daa0a116cd9bf
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-13 13:35:02 -0300

    feat: add header mobile sheet menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,11 +8,25 @@ import {
   Link,
   Button,
   Image,
+  Divider,
 } from "@nextui-org/react";
+
 import { ImEnter } from "react-icons/im";
+import { RxDashboard } from "react-icons/rx";
+import { MdMenu } from "react-icons/md";
+import { RiPagesLine } from "react-icons/ri";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Header = () => {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, user, isLoaded } = useUser();
 
   return (
     <Navbar
@@ -48,8 +62,78 @@ const Header = () => {
               Log In
             </Button>
           ) : (
-            <div className="border-2 border-primary rounded-full">
-              <UserButton />
+            <div className="flex items-center gap-5">
+              <nav className="flex items-center gap-5">
+                <div className="rounded-full border-2 border-primary">
+                  <UserButton />
+                </div>
+
+                <Button
+                  as={Link}
+                  color="primary"
+                  href="/dashboard"
+                  variant="shadow"
+                  className="hidden text-white md:flex"
+                  endContent={<RxDashboard size={25} />}
+                  radius="sm"
+                >
+                  Dashboard
+                </Button>
+              </nav>
+
+              <Sheet>
+                <SheetTrigger asChild className="md:hidden">
+                  <Button
+                    color="primary"
+                    variant="shadow"
+                    className="text-white"
+                    endContent={<MdMenu size={30} />}
+                    radius="sm"
+                    isIconOnly
+                  />
+                </SheetTrigger>
+                <SheetContent>
+                  <SheetHeader>
+                    <SheetTitle className="mb-5 flex justify-center">
+                      <Image
+                        src="/Logo.svg"
+                        alt="TRIPSYNC"
+                        width={0}
+                        height={0}
+                        style={{ objectFit: "contain" }}
+                        className="h-auto w-44"
+                      />
+                    </SheetTitle>
+                    <Divider />
+                    <SheetDescription className="w-full">
+                      <nav className="mt-5 flex flex-col items-center justify-center gap-3">
+                        <Button
+                          as={Link}
+                          color="primary"
+                          href="/"
+                          variant="shadow"
+                          className="w-full text-white"
+                          endContent={<RiPagesLine size={25} />}
+                          radius="sm"
+                        >
+                          Homepage
+                        </Button>
+                        <Button
+                          as={Link}
+                          color="primary"
+                          href="/dashboard"
+                          variant="shadow"
+                          className="w-full text-white"
+                          endContent={<RxDashboard size={25} />}
+                          radius="sm"
+                        >
+                          Dashboard
+                        </Button>
+                      </nav>
+                    </SheetDescription>
+                  </SheetHeader>
+                </SheetContent>
+              </Sheet>
             </div>
           )}
         </NavbarItem>
