d07263b0f052e9839836a385c2e0ac8649d56fba
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-13 23:25:09 -0300

    style: change spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
-import { ClipLoader } from "react-spinners";
+import { Spinner } from "@nextui-org/react";
 const Loading = () => {
   return (
     <div className="flex min-h-screen w-full flex-grow flex-col items-center justify-center bg-[url('/loading.svg')] bg-cover bg-center bg-no-repeat">
-      <ClipLoader color="#1BB2B5" size={40} />
+      <Spinner label="Loading..." color="primary" />
     </div>
   );
 };
