71222746c2e11b3e364bbcecd517f81e6696add4
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-14 18:12:48 -0300

    style: remove spinner label

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@nextui-org/react";
 const Loading = () => {
   return (
     <div className="flex min-h-screen w-full flex-grow flex-col items-center justify-center bg-[url('/loading.svg')] bg-cover bg-center bg-no-repeat">
-      <Spinner label="Loading..." color="primary" />
+      <Spinner color="primary"/>
     </div>
   );
 };
