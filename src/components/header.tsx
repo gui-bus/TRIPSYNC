0ce4fa0c4cc3b8a92b126f744a93d12f4f233ae9
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-15 14:58:00 -0300

    feat: change header menu from sheets to dropdown

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,148 +1,187 @@
 "use client";
-import { UserButton, useUser } from "@clerk/nextjs";
+import { useClerk, useUser } from "@clerk/nextjs";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
-  NavbarItem,
   Link,
-  Button,
   Image,
   Divider,
+  Dropdown,
+  DropdownTrigger,
+  Avatar,
+  DropdownMenu,
+  DropdownItem,
 } from "@nextui-org/react";
 
-import { ImEnter } from "react-icons/im";
 import { RxDashboard } from "react-icons/rx";
-import { MdMenu } from "react-icons/md";
+import { MdPublic } from "react-icons/md";
 import { RiPagesLine } from "react-icons/ri";
 import { TbScriptPlus, TbScript } from "react-icons/tb";
 
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { LuLogOut } from "react-icons/lu";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
-  const { isSignedIn, user, isLoaded } = useUser();
+  const { isSignedIn, user } = useUser();
+  const { signOut } = useClerk();
+  const router = useRouter();
 
   return (
     <Navbar
       position="sticky"
       isBlurred={false}
       isBordered
-      className="drop-shadow-md"
+      className="w-full drop-shadow-md"
     >
-      <NavbarBrand>
-        <Link href="/">
-          <Image
-            src="/Logo.svg"
-            alt="TRIPSYNC"
-            width={0}
-            height={0}
-            style={{ objectFit: "contain" }}
-            className="h-auto w-44"
-          />
-        </Link>
-      </NavbarBrand>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          {!isSignedIn ? (
-            <Button
-              as={Link}
-              color="primary"
-              href="/sign-in"
-              variant="shadow"
-              className="text-white"
-              endContent={<ImEnter size={25} />}
-              radius="sm"
-            >
-              Log In
-            </Button>
+      <div className="mx-auto flex w-full max-w-xs sm:max-w-md lg:max-w-7xl items-center">
+        <NavbarBrand>
+          <Link href="/">
+            <Image
+              src="/Logo.svg"
+              alt="TRIPSYNC"
+              width={0}
+              height={0}
+              style={{ objectFit: "contain" }}
+              className="h-auto w-44"
+            />
+          </Link>
+        </NavbarBrand>
+        <NavbarContent justify="end">
+          {isSignedIn ? (
+            <Dropdown placement="bottom">
+              <DropdownTrigger>
+                <Avatar
+                  isBordered
+                  as="button"
+                  className="transition-transform"
+                  color="primary"
+                  name={user?.firstName || ""}
+                  size="sm"
+                  src={user?.imageUrl}
+                />
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label="Profile Actions"
+                variant="shadow"
+                color="secondary"
+                disabledKeys={["user-info", "divider"]}
+              >
+                <DropdownItem className="h-14 gap-2" key="user-info">
+                  <p className="font-semibold">{user?.fullName}</p>
+                  <p className="text-xs font-semibold">
+                    {String(user?.primaryEmailAddress)}
+                  </p>
+                </DropdownItem>
+                <DropdownItem
+                  startContent={<RiPagesLine />}
+                  onClick={() => router.push("/")}
+                  color="primary"
+                >
+                  Homepage
+                </DropdownItem>
+
+                <DropdownItem
+                  startContent={<MdPublic />}
+                  onClick={() => router.push("/adventure/public")}
+                  color="primary"
+                >
+                  View Public Plans
+                </DropdownItem>
+
+                <DropdownItem key="divider">
+                  <Divider />
+                </DropdownItem>
+
+                <DropdownItem
+                  startContent={<RxDashboard />}
+                  onClick={() => router.push("/dashboard")}
+                  color="primary"
+                >
+                  Dashboard
+                </DropdownItem>
+
+                <DropdownItem
+                  startContent={<TbScript />}
+                  onClick={() => router.push("/adventure/list")}
+                  color="primary"
+                >
+                  My Vacations Plans
+                </DropdownItem>
+
+                <DropdownItem
+                  startContent={<TbScriptPlus />}
+                  onClick={() => router.push("/adventure/new")}
+                  color="primary"
+                >
+                  Add Vacation Plan
+                </DropdownItem>
+
+                <DropdownItem key="divider">
+                  <Divider />
+                </DropdownItem>
+
+                <DropdownItem
+                  startContent={<LuLogOut />}
+                  key="logout"
+                  color="danger"
+                  onClick={() => signOut(() => router.push("/"))}
+                >
+                  Logout
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
           ) : (
-            <div className="flex items-center gap-5">
-              <div className="rounded-full border-2 transition-all duration-200 ease-in-out hover:border-primary">
-                <UserButton />
-              </div>
+            <Dropdown placement="bottom">
+              <DropdownTrigger>
+                <Avatar
+                  isBordered
+                  as="button"
+                  className="transition-transform"
+                  color="primary"
+                  name=""
+                  size="sm"
+                  src=""
+                />
+              </DropdownTrigger>
+              <DropdownMenu
+                aria-label="Profile Actions"
+                variant="shadow"
+                color="secondary"
+                disabledKeys={["divider"]}
+              >
+                <DropdownItem
+                  startContent={<RiPagesLine />}
+                  onClick={() => router.push("/")}
+                  color="primary"
+                >
+                  Homepage
+                </DropdownItem>
 
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button
-                    variant="light"
-                    endContent={<MdMenu size={30} />}
-                    radius="sm"
-                    isIconOnly
-                  />
-                </SheetTrigger>
-                <SheetContent>
-                  <SheetHeader>
-                    <SheetTitle className="mb-5 flex justify-center">
-                      <Image
-                        src="/Logo.svg"
-                        alt="TRIPSYNC"
-                        width={0}
-                        height={0}
-                        style={{ objectFit: "contain" }}
-                        className="h-auto w-44"
-                      />
-                    </SheetTitle>
-                    <Divider />
-                    <SheetDescription className="w-full">
-                      <nav className="mt-5 flex flex-col items-center justify-center gap-3">
-                        <Button
-                          as={Link}
-                          href="/"
-                          variant="ghost"
-                          className="w-full"
-                          endContent={<RiPagesLine size={25} className="opacity-80"/>}
-                          radius="sm"
-                        >
-                          Homepage
-                        </Button>
+                <DropdownItem
+                  startContent={<MdPublic />}
+                  onClick={() => router.push("/adventure/public")}
+                  color="primary"
+                >
+                  View Public Plans
+                </DropdownItem>
 
-                        <Button
-                          as={Link}
-                          href="/dashboard"
-                          variant="ghost"
-                          className="w-full"
-                          endContent={<RxDashboard size={25} className="opacity-80"/>}
-                          radius="sm"
-                        >
-                          Dashboard
-                        </Button>
+                <DropdownItem key="divider">
+                  <Divider />
+                </DropdownItem>
 
-                        <Button
-                          as={Link}
-                          href="/adventure/list"
-                          variant="ghost"
-                          className="w-full"
-                          endContent={<TbScript size={25} className="opacity-80"/>}
-                          radius="sm"
-                        >
-                          My vacation plans
-                        </Button>
-                        <Button
-                          as={Link}
-                          href="/adventure/new"
-                          variant="ghost"
-                          className="w-full"
-                          endContent={<TbScriptPlus size={25} className="opacity-80"/>}
-                          radius="sm"
-                        >
-                          Add new vacation plan
-                        </Button>
-                      </nav>
-                    </SheetDescription>
-                  </SheetHeader>
-                </SheetContent>
-              </Sheet>
-            </div>
+                <DropdownItem
+                  startContent={<LuLogOut />}
+                  onClick={() => router.push("/sign-in")}
+                  color="primary"
+                >
+                  Log In
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
           )}
-        </NavbarItem>
-      </NavbarContent>
+        </NavbarContent>
+      </div>
     </Navbar>
   );
 };
