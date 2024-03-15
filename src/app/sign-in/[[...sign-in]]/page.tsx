30d165327cdcbc15d414f23f61adb52e7e189f1b
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-03-15 16:38:29 -0300

    style: change bg img

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex py-20 w-full items-center justify-center bg-[url('/loading.svg')] bg-cover bg-center bg-no-repeat">
+    <div className="flex py-20 w-full items-center justify-center bg-[url('/Loading.svg')] bg-cover bg-center bg-no-repeat">
       <SignIn />
     </div>
   );
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex py-20 w-full flex-col items-center justify-center bg-[url('/loading.svg')] bg-cover bg-center bg-no-repeat">
+    <div className="flex py-20 w-full flex-col items-center justify-center bg-[url('/Loading.svg')] bg-cover bg-center bg-no-repeat">
       <SignUp />
     </div>
   );
